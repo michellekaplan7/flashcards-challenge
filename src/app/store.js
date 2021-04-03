4da43403d404d0a09b9cc71f608aba0d29673575
Michelle Kaplan <kaplan.michelle7@gmail.com>
2021-04-02 23:59:54 -0600

    Write categoryReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import categoryReducer from "../components/CategorySelection/categorySlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    category: categoryReducer,
   },
 });
