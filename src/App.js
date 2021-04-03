c4b0aad86ffb34dc6a1316b4e3c2f7e25d1d3e56
Michelle Kaplan <kaplan.michelle7@gmail.com>
2021-04-02 23:59:32 -0600

    Render Header, CategorySelection, and CardsContainer in App

--- src/App.js
+++ src/App.js
@@ -1,58 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectCategory } from "./components/CategorySelection/categorySlice";
+import "./App.css";
 
-function App() {
+import Header from "./components/Header/Header";
+import CategorySelection from "./components/CategorySelection/CategorySelection";
+import CardsContainer from "./components/CardsContainer/CardsContainer";
+
+const App = () => {
+  const category = useSelector(selectCategory);
+  console.log({ category });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div>
+        <CardsContainer />
+
+        {/* {!category ? (
+          <CategorySelection />
+        ) : (
+          <h2 className="category-header">
+            Your selected category is <span>{`< ${category} >`}</span>
+          </h2>
+        )} */}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
