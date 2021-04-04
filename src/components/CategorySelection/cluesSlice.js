3f3b923b7903e0bdb70e6eae79eeb4ada24a7b18
Michelle Kaplan <kaplan.michelle7@gmail.com>
2021-04-03 20:58:22 -0600

    Update cluesSlice to implement createAsyncThunk to fetch clues from API

---
+++ src/components/CategorySelection/cluesSlice.js
@@ -0,0 +1,43 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getClues = createAsyncThunk(
+  "clues/getClues",
+  async (id, { dispatch, getState }) => {
+    console.log({ id });
+    return fetch(`http://jservice.io/api/clues?category=${id}`).then((res) =>
+      res.json()
+    );
+  }
+);
+const cluesSlice = createSlice({
+  name: "clues",
+  initialState: {
+    list: [],
+    status: null,
+  },
+  extraReducers: {
+    [getClues.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [getClues.fulfilled]: (state, { payload }) => {
+      state.list = payload.map((clue) => {
+        return {
+          id: clue.id,
+          answer: clue.answer,
+          question: clue.question,
+          value: clue.value,
+          categoryID: clue.category_id,
+        };
+      });
+
+      state.status = "success";
+    },
+    [getClues.rejected]: (state, action) => {
+      state.status = "failed";
+    },
+  },
+});
+
+export default cluesSlice.reducer;
+
+export const clues = (state) => state.clues.list;
