bb97a62febd2430f8d72a835f1548c555921b4fb
Michelle Kaplan <kaplan.michelle7@gmail.com>
2021-04-04 23:19:42 -0600

    Add userSlice reducers and state

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -3,20 +3,39 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: "userInfo",
   initialState: {
-    correctAnswers: null,
-    incorrectAnswers: [],
+    answers: {
+      correctAnswers: [],
+      incorrectAnswers: [],
+    },
+
+    bank: 0,
   },
   reducers: {
-    userSelectCorrectAnswer(state, action) {
-      state.correctAnswers = action.payload;
+    userSelectCorrectAnswer(state, { payload }) {
+      state.answers.correctAnswers.push(payload);
+    },
+    userSelectIncorrectAnswer(state, { payload }) {
+      state.answers.incorrectAnswers.push(payload);
+    },
+    increaseUserBank(state, action) {
+      state.bank += action.payload;
+    },
+    decreaseUserBank(state, action) {
+      state.bank -= action.payload;
     },
   },
 });
 
 export default userSlice.reducer;
 
-// export const selectCategoriesList = (state) => state.categories.list;
+export const correctAnswers = (state) => state.userInfo.answers.correctAnswers;
+export const incorrectAnswers = (state) =>
+  state.userInfo.answers.incorrectAnswers;
+export const bankAmount = (state) => state.userInfo.bank;
 
-// export const currentCategory = (state) => state.categories.currentCategory;
-
-export const { userSelectCorrectAnswer } = userSlice.actions;
+export const {
+  userSelectCorrectAnswer,
+  userSelectIncorrectAnswer,
+  increaseUserBank,
+  decreaseUserBank,
+} = userSlice.actions;
